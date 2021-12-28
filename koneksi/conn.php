450e59f974aaaea33cb91ca688a4243c22ed7602
ALVIRAHMADHANI01 <alvirahmadhani01@gmail.com>
2021-12-28 22:35:55 +0700

    fix database

--- koneksi/conn.php
+++ koneksi/conn.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Jakarta");
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "db_tiketpesawat";
+$dbname = "tiketpesawat";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
